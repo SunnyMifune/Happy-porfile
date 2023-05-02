14bf1ffb6a83c96cf660d9224a53b976774c8621
sunny liu <372930@gmail.com>
2023-05-02 23:07:26 +0900

    skill update

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,10 @@
 import { Container, Col, Row } from "react-bootstrap";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import meter1 from "../assets/img/meter1.svg";
+import meter2 from "../assets/img/meter2.svg";
+import meter3 from "../assets/img/meter3.svg";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
@@ -35,7 +39,19 @@ export const Skills = () => {
               <p>lorem</p>
               <Carousel responsive={responsive} infinite={true} className="skill-slider">
               <div className="item">
-                <img src={} alt="Image" />
+                <img src={meter1} alt="Image" />
+                <h5>Web Development</h5>
+              </div>
+              <div className="item">
+                <img src={meter2} alt="Image" />
+                <h5>Brand Identity</h5>
+              </div>
+              <div className="item">
+                <img src={meter3} alt="Image" />
+                <h5>Logo Design</h5>
+              </div>
+              <div className="item">
+                <img src={meter1} alt="Image" />
                 <h5>Web Development</h5>
               </div>
               </Carousel>
@@ -43,6 +59,7 @@ export const Skills = () => {
           </Col>
         </Row>
       </Container>
+      <img className="background-image-left" src={colorSharp} />
     </section>
   )
 }
