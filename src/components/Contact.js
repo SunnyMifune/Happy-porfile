39e30980bfca8d7eb4d2fb2beddf55dc6821ba17
sunny liu <372930@gmail.com>
2023-05-19 17:35:40 +0900

    updated the column

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,6 +15,8 @@ export const Contact = () => {
   const [buttonText, setButtonText] = useState('Send');
   const [status, setStatus] = useState({});
 
+  const onFormUpdate = (category, value) =>
+
   return (
     <section className="contact" id="connect">
       <Container>
@@ -26,7 +28,19 @@ export const Contact = () => {
             <h2>Get In Touch</h2>
             <form>
               <Row>
-                <Col sm={6} className="px-1"></Col>
+                <Col sm={6} className="px-1">
+                  <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName', e.target.value)} />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName', e.target.value)} />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email', e.target.value)} />
+                </Col>
+                <Col sm={6} className="px-1">
+                  <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)} />
+                </Col>
+                <Col></Col>
               </Row>
             </form>
           </Col>
