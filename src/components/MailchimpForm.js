21b8978945108831603191a52268baf825f10166
sunny liu <372930@gmail.com>
2023-06-08 23:58:41 +0800

    add posturl

--- src/components/MailchimpForm.js
+++ src/components/MailchimpForm.js
@@ -2,4 +2,15 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 
 export const mailchimpForm = () => {
   const postUrl = '$(process.env.REACT_APP_MAILCHIMP_URL)?u=$(process.env.REACT_APP_MAILCHIMP_U)$id=$(process.env.REACT_APP_MAILCHIMP_ID)'
+
+  return (
+    <>
+      <MailchimpSubscribe
+        url={postUrl}
+        render={({ subscribe, status, message }) => (
+          <></>
+        )}
+        />
+    </>
+  )
 }
