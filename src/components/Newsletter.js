90b67277292ad9ff62026ef11c2c00b77e7e7011
sunny liu <372930@gmail.com>
2023-06-12 22:58:00 +0800

    updated newsletter

---
+++ src/components/Newsletter.js
@@ -0,0 +1,16 @@
+import { Alert } from "react-bootstrap"
+
+export const Newsletter = ({ subscribe, status, message }) => {
+  return (
+    <Col lg={12}>
+      <div className="newsletter-bx">
+        <Row>
+          <Col lg={12} md={6} xl={5}>
+            <h3>Subscribe to our Newsletter</h3>
+            {status === 'sending' && <Alert>Sending...</Alert>}
+          </Col>
+        </Row>
+      </div>
+    </Col>
+  )
+}
