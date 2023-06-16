7703c7c12f56bdfe52ddb8dc025961faa1efc6ae
sunny liu <372930@gmail.com>
2023-06-16 23:58:42 +0900

    add email

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,6 +1,12 @@
-import { Alert } from "react-bootstrap"
+import { Alert } from "react-bootstrap";
+import { useState } from "react";
 
 export const Newsletter = ({ subscribe, status, message }) => {
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+
+  }
   return (
     <Col lg={12}>
       <div className="newsletter-bx">
@@ -10,6 +16,13 @@ export const Newsletter = ({ subscribe, status, message }) => {
             {status === 'sending' && <Alert>Sending...</Alert>}
             {status === 'error' && <Alert variant="danger">{message}</Alert>}
             {status === 'success' && <Alert variant="success">{message}</Alert>}
+          </Col>
+          <Col md={6} xl={7}>
+            <form onSubmit={handleSubmit}>
+              <div className="new-email-bx">
+                <input value={email} type="email" onChange={(e) => setEmail(e.target.value)} placeholder="Email Address"/>
+              </div>
+            </form>
           </Col>
         </Row>
       </div>
