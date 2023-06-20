af7c970832ddfe96ae79fdaf62f0c9c506083156
sunny liu <372930@gmail.com>
2023-06-20 23:44:48 +0900

    add container in footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap"
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        
+      </Container>
+    </footer>
+  )
+}
