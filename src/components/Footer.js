3bc9044ab076ba0cecefc4359c8cc482e218d754
sunny liu <372930@gmail.com>
2023-06-21 23:23:07 +0900

    add mailchimpform in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
-import { Container } from "react-bootstrap"
+import { Container } from "react-bootstrap";
+import { MailchimpForm } from "./MailchimpForm";
+import logo from "../assets/img/logo.svg";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        
+        <Row className="align-item-center">
+          <MailchimpForm />
+          <Col sm={6}>
+            <img src={logo} alt="Logo" />
+          </Col>
+        </Row>
       </Container>
     </footer>
   )
