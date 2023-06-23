98f25eb508b89f701678486bf99be7fde0a57c87
sunny liu <372930@gmail.com>
2023-06-23 23:11:05 +0900

    add nav icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import { Container } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const Footer = () => {
   return (
@@ -10,6 +13,11 @@ export const Footer = () => {
           <MailchimpForm />
           <Col sm={6}>
             <img src={logo} alt="Logo" />
+          </Col>
+          <Col sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a href=""><img src={} /></a>
+            </div>
           </Col>
         </Row>
       </Container>
