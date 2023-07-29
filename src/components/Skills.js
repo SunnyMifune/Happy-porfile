10119b6688fdee5f94d62432e228518485168974
sunny liu <372930@gmail.com>
2023-07-29 23:52:34 +0900

    update the skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,10 +1,11 @@
-import { Container, Col, Row } from "react-bootstrap";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import colorSharp from "../assets/img/color-sharp.png";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import arrow1 from "../assets/img/arrow1.svg";
+import arrow2 from "../assets/img/arrow2.svg";
+import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
   const responsive = {
@@ -29,37 +30,35 @@ export const Skills = () => {
 
   return (
     <section className="skill" id="skills">
-      <Container>
-        <Row>
-          <Col>
-            <div className="skill-bx">
-              <h2>
-                Skills
-              </h2>
-              <p>lorem</p>
-              <Carousel responsive={responsive} infinite={true} className="skill-slider">
-              <div className="item">
-                <img src={meter1} alt="Image" />
-                <h5>Web Development</h5>
-              </div>
-              <div className="item">
-                <img src={meter2} alt="Image" />
-                <h5>Brand Identity</h5>
-              </div>
-              <div className="item">
-                <img src={meter3} alt="Image" />
-                <h5>Logo Design</h5>
-              </div>
-              <div className="item">
-                <img src={meter1} alt="Image" />
-                <h5>Web Development</h5>
-              </div>
-              </Carousel>
+        <div className="container">
+            <div className="row">
+                <div className="col-12">
+                    <div className="skill-bx wow zoomIn">
+                        <h2>Skills</h2>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br></br> Lorem Ipsum has been the industry's standard dummy text.</p>
+                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter2} alt="Image" />
+                                <h5>Brand Identity</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter3} alt="Image" />
+                                <h5>Logo Design</h5>
+                            </div>
+                            <div className="item">
+                                <img src={meter1} alt="Image" />
+                                <h5>Web Development</h5>
+                            </div>
+                        </Carousel>
+                    </div>
+                </div>
             </div>
-          </Col>
-        </Row>
-      </Container>
-      <img className="background-image-left" src={colorSharp} />
+        </div>
+        <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
