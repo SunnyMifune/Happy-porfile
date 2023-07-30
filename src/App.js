9d8419d9744ce5361698fea38b70a3a2305960ea
sunny liu <372930@gmail.com>
2023-07-30 23:35:05 +0900

    add contact in the app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
-import { Footer } from "./components/Footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Contact } from "./components/Contact";
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
       <Banner />
       <Skills />
       <Projects />
+      <Contact />
       <Footer />
     </div>
   );
